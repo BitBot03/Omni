867d925feba2f465a08a22c11e188f72e8f148e9
Temporary User <temporary@example.com>
2026-04-28 16:20:11 +0530

    Delete all files from repo

--- app/applet/fix.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('/app/applet/omni-fitness/js/tabs/nutrition.js', 'utf8');
-c = c.replace(/\\\${/g, '${');
-fs.writeFileSync('/app/applet/omni-fitness/js/tabs/nutrition.js', c);
